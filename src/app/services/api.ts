6cd8a622be7a826f1362b76a3d5b1b40cbf1a57d
AtplApplicationsGitHub <66077941+AtplApplicationsGitHub@users.noreply.github.com>
2025-07-25 15:36:46 +0530

    Update api.ts

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -12,7 +12,7 @@ import {
 // Create axios instance
 const api = axios.create({
   // baseURL: 'https://dev.goval.app:2083/api',
-  baseURL: "https://emp.onboarding.goval.app:8084/api",
+  baseURL: "https://employee.onboarding.goval.app:8084/api",
   headers: {
     "Content-Type": "application/json",
   },
